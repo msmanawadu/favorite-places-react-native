83112a360ad966af98ffea0bea4168ab08e2e115
msmanawadu <msmanawadu@gmail.com>
2025-01-13 14:35:40 +0530

    Setting up routing and navigation

---
+++ components/Places/PlaceItem.js
@@ -0,0 +1,16 @@
+import { Pressable, Image, View, Text, StyleSheet } from 'react-native';
+function PlaceItem({ place, onSelect }) {
+	return (
+		<Pressable onPress={onSelect}>
+			<Image source={{ uri: place.imageUri }} />
+			<View>
+				<Text>{place.title}</Text>
+				<Text>{place.address}</Text>
+			</View>
+		</Pressable>
+	);
+}
+
+const styles = StyleSheet.create({});
+
+export default PlaceItem;
